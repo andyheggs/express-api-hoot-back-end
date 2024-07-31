0823b9a9be0f0805cdb58b014477d78fe1edef2f
Andy Heggs <Andy@Heggs.net>
2024-07-31 09:34:42 +0100

    init commit

---
+++ controllers/test-jwt.js
@@ -0,0 +1,42 @@
+
+const express = require('express');
+const router = express.Router();
+
+const jwt = require('jsonwebtoken');
+
+
+//--------------------------------GET - SIGN IN JWT TOKEN CREAITON ROUTE---------------------------------------------------//
+
+router.get('/sign-token', (req, res) => {
+    // Mock user object added
+    const user = {
+      _id: 1,
+      username: 'test',
+      password: 'test',
+    };
+  
+
+const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+res.json({ token });
+
+});
+
+//--------------------------------POST - SIGN IN TOKEN VERIFICATION ROUTE---------------------------------------------------//
+
+
+router.post('/verify-token', (req, res) => {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      // Add in verify method
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      res.json({ decoded });
+    } catch (error) {
+      res.status(401).json({ error: 'Invalid token.' });
+    }
+  });
+
+
+
+
+module.exports = router;
